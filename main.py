54e876d93f24522a539e0d65b719572cf7259d29
minh-ctrl <binhminh140899@gmail.com>
2022-10-26 00:32:15 +0900

    script

---
+++ main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+html = open('Max.html', 'r')
+soup = BeautifulSoup(html, 'html.parser')
+if len(soup.find_all('div', class_='display-flex ph5 pv3')) > 0:
+    About = soup.find_all('div', class_='display-flex ph5 pv3')[0].get_text()
+
+Sections = soup.find_all(
+    'section', class_='artdeco-card ember-view relative break-words pb3 mt2')
+relInfo = []
+for x in Sections:
+    if "AboutAbout" in x.get_text():
+        About = x.get_text()
+        relInfo.append(About)
+    if "ExperienceExperience" in x.get_text():
+        Experience = x.get_text()
+        relInfo.append(Experience)
+    if "VolunteeringVolunteering" in x.get_text():
+        Volunteer = x.get_text()
+        relInfo.append(Volunteer)
+    if "SkillsSkills" in x.get_text():
+        Skills = x.get_text()
+        relInfo.append(Skills)
+    if "LanguagesLanguages" in x.get_text():
+        Languages = x.get_text()
+        relInfo.append(Languages)
+print(About)

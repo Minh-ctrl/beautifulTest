622ffd90d6f9b5440ad44d6d75e9261d50a6670a
minh-ctrl <binhminh140899@gmail.com>
2022-10-28 01:33:06 +0900

    added job descriptions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 import json
-html = open('Tuan.html', 'r')
+html = open('Minh.html', 'r')
 soup = BeautifulSoup(html, 'html.parser')
 Sections = soup.find_all(
     'section', class_='artdeco-card ember-view relative break-words pb3 mt2')
@@ -20,47 +20,59 @@ for x in Sections:
             'About': about.get_text()
         })
     if "Experience" in x.find('div', class_='pvs-header__top-container--no-stack').get_text():
-        # Titles = x.find_all(
-        #     'div', class_='display-flex align-items-center')
-        # Companies = x.find_all('span', class_='t-14 t-normal')
-        # if len(Companies) == len(Titles):
-        #     for f, b in zip(Companies, Titles):
-        #         # clean repeatable
-        #         company = f.find('span', class_='visually-hidden')
-        #         title = b.find('span', class_='visually-hidden')
-        #         experience = company.get_text() + ' ' + title.get_text()
-        #         experiences.update({company.get_text(): title.get_text()})
-
         jobs = []
+        periods = []
+        about = None
         blocks = x.find_all(
             'div', class_='pvs-entity pvs-entity--padded pvs-list__item--no-padding-when-nested')
         for block in blocks:
             Titles = block.find_all(
                 'div', class_='display-flex align-items-center')
+            Times = block.find_all(
+                'span', class_='t-14 t-normal t-black--light'
+            )
+            Descriptions = block.find_all(
+                'div', class_='inline-show-more-text')
             Companies = block.find_all('span', class_='t-14 t-normal')
-            # Company = Companies.get_text()
+            for Description in Descriptions:
+                about = Description.get_text()
             for company in Companies:
                 name = company.find('span', class_='visually-hidden')
             if len(Titles) > 1:
+                for Time in Times:
+                    period = Time.find(
+                        'span', class_='visually-hidden').get_text()
+                    periods.append(period + ' ')
+                workPeriod = ''.join(periods)
                 for title in Titles:
                     job = title.find('span', class_='visually-hidden')
                     jobs.append(job.get_text())
 #       very lazy workaround
                 popped = jobs.pop(0)
-                experiences.update({popped: jobs})
+                experiences.update(
+                    {popped: [{'roles': jobs, 'Periods': workPeriod, 'Description': about}]})
             else:
                 for title in Titles:
+                    for Time in Times:
+                        period = Time.find(
+                            'span', class_='visually-hidden').get_text()
+                        periods.append(period + ' ')
+                    workPeriod = ''.join(periods)
                     job = title.find(
                         'span', class_='visually-hidden').get_text()
-                    experiences.update({name.get_text(): job})
+                    experiences.update(
+                        {name.get_text(): [{'role': job, 'Period': workPeriod, 'Description': about}]})
+                    periods = []
+
+                    # workPeriod = ''
         relInfo.update({
             'Experiences': experiences
         })
 
-    if "Volunteering" in x.find('div', class_='pvs-header__top-container--no-stack').get_text():
-        relInfo.update({
-            'Volunteer': x.get_text(strip=True)
-        })
+    # if "Volunteering" in x.find('div', class_='pvs-header__top-container--no-stack').get_text():
+    #     relInfo.update({
+    #         'Volunteer': x.get_text(strip=True)
+    #     })
     if "Skills" in x.find('div', class_='pvs-header__top-container--no-stack').get_text():
         skills = x.find_all(
             'div', class_="display-flex align-items-center")
@@ -84,6 +96,7 @@ for x in Sections:
         Schools = x.find_all('div', class_='display-flex align-items-center')
         # Periods = x.find_all('span', class_='t-14 t-normal t-black--light')
         Types = x.find_all('span', class_='t-14 t-normal')
+        # zip data structure might not be appropriate here
         for f, b in zip(Schools, Types):
             schools = f.find('span', class_='mr1 hoverable-link-text t-bold')
             school = schools.find('span', class_='visually-hidden')
@@ -92,11 +105,7 @@ for x in Sections:
         relInfo.update({
             'Educations': Educations
         })
-        #         # clean repeatable
-        #         company = f.find('span', class_='visually-hidden')
-        #         title = b.find('span', class_='visually-hidden')
-        #         experience = company.get_text() + ' ' + title.get_text()
-        #         experiences.update({company.get_text(): title.get_text()})
+
 res = json.dumps(relInfo, ensure_ascii=False)
 file = open("test.json", "w")
 file.write(res)
